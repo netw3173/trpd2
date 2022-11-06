9893ac74db248e728dd2a5c63f25701c6e51c895
netw3173 <113054391+netw3173@users.noreply.github.com>
2022-11-06 10:55:55 -0500

    Add files via upload

---
+++ CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/unbuntu/bmorecoin/src
+# Build directory: /home/unbuntu/bmorecoin/build/release/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
